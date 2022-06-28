188b0bfcc8824c46c374a95b7d85c487ff75290b
ferdianada <ferdiannada@gmail.com>
2022-06-28 09:09:51 +0700

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+let kalimat = "Hello  World"; //string
+let angka = 25; //integer
+let is_true = true; //boolean
+let decimal = 1.2; //double
+let motor = ["Yamaha", "Honda", "Suzuk"]; //array
+let biodata = { first_name: "rezky", last_name: "putra", age: 24 }; //object
