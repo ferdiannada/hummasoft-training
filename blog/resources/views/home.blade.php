2bbe59512d756740ccb2b7c42330d3b62b098256
ferdianada <ferdiannada@gmail.com>
2022-06-29 11:16:39 +0700

    make views - laravel

---
+++ blog/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+@section('content')
+<h1>Hello, world!</h1>
+<button class="btn btn-outline-success">Jos!</button>
+@endsection
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
-});
+    $title = "Home";
+    return view('home', ["title" => $title]);
+})->name('home');
+
+Route::get('/admin', function () {
+    $title = "Admin";
+    return view('admin', ["title" => $title]);
+})->name('admin');
+
+
+// get : untuk menampilkan halaman atau data saja
+// post : untuk mengirimkan data
+// put : untuk mengubah data
+// patch : wajib untuk mengubah semua data
+// delete : untuk menghapus data
